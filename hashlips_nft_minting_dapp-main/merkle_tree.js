ca91fe2077ec82f88c8fa875ffd9d70b035daa31
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-01-23 16:39:09 +0900

    create a merkletree

---
+++ hashlips_nft_minting_dapp-main/merkle_tree.js
@@ -0,0 +1,13 @@
+const { MerkleTree } = require("merkletreejs");
+const keccak256 = require('keccak256');
+
+let allowlistAddresses = [
+    "0x448bc68105f067AF52A4AC0d1e2Dd5d3F77a1271",
+    "0x1f595fBCB116EF3C19CBF2530dF14a5C0A15cbAF",
+];
+
+const leafNodes = allowlistAddresses.map(address => keccak256(address));
+const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+
+console.log(leafNodes)
+console.log(merkleTree)
