f5cf7333261a458010ef097af4cdb88d5fe2e730
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-01-24 12:39:59 +0900

    Using ether's keccak256 algorithm to hash the two arguments

--- hashlips_nft_minting_dapp-main/merkle_tree.js
+++ hashlips_nft_minting_dapp-main/merkle_tree.js
@@ -1,23 +1,25 @@
+const { ethers } = require("ethers");
 const { MerkleTree } = require("merkletreejs");
 const keccak256 = require('keccak256');
 
 let allowlistAddresses = [
-    "0x448bc68105f067AF52A4AC0d1e2Dd5d3F77a1271",
-    "0x1f595fBCB116EF3C19CBF2530dF14a5C0A15cbAF",
+    ["0x448bc68105f067AF52A4AC0d1e2Dd5d3F77a1271",3],
+    ["0x1f595fBCB116EF3C19CBF2530dF14a5C0A15cbAF",2],
 ];
 
-const leafNodes = allowlistAddresses.map(address => keccak256(address));
+const leafNodes = allowlistAddresses.map(addr => ethers.utils.solidityKeccak256(['address', 'uint256'], [addr[0], addr[1]]));
 const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
-// console.log(leafNodes);
-// console.log(merkleTree);
+console.log("leafNode ; \n", leafNodes);
+console.log(merkleTree);
 
 const rootHash = merkleTree.getRoot();
-// console.log("Allowlist Merkle Tree\n", merkleTree.toString());
-// console.log("Root Hash", rootHash);
+console.log("Allowlist Merkle Tree\n", merkleTree.toString());
+console.log("Root Hash", "0x" + rootHash.toString("hex"));
 
-const claiminingAddress = leafNodes[2];
+const claiminingAddress = ethers.utils.solidityKeccak256(['address', 'uint256'], [allowlistAddresses[0][0], allowlistAddresses[0][1]]);
+console.log(claiminingAddress)
+
 const hexProof = merkleTree.getHexProof(claiminingAddress);
-
-console.log(hexProof, claiminingAddress, rootHash)//ここから始める
-
+console.log("hexProof ; \n", hexProof)
 console.log(merkleTree.verify(hexProof, claiminingAddress, rootHash))
+
