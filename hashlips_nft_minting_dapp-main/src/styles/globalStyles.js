c971b01a52def88c04beab8a17e413522f67250a
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2023-03-10 12:14:41 +0900

    Update globalStyles.js

--- hashlips_nft_minting_dapp-main/src/styles/globalStyles.js
+++ hashlips_nft_minting_dapp-main/src/styles/globalStyles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 // Used for wrapping a page component
 export const Screen = styled.div`
-  background-color: var(--primary);
   background-image: ${({ image }) => (image ? `url(${image})` : "none")};
   background-size: cover;
   background-position: center;
@@ -10,6 +9,11 @@ export const Screen = styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
+  opacity: 0.85;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  position: fixed;
 `;
 
 // Used for providing space between components
@@ -74,3 +78,73 @@ export const StyledClickable = styled.div`
     opacity: 0.6;
   }
 `;
+
+export const StyledLogo = styled.div`
+  width: 100%;
+  text-align: center;
+  margin-top: 10px;
+  font-size: 48px;
+  color: #ffffff;
+  text-shadow: 0 5px #000000;
+  font-family: "Press Start 2P";
+  word-wrap: break-word; /* 折り返しを行う */ 
+`;
+
+export const MintNumber = styled.p`
+  color: #000000;
+  font-size: 16px;
+  line-height: 1.6;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 40px;
+`;
+
+export const LeftSideSection = styled.div`
+  display: flex;
+  align: center;
+`;
+
+export const RightSideSection = styled.div`
+  display: flex;
+  align: center;
+  color: #ffffff;
+`;
+
+export const Icon = styled.div`
+  width: 42px;
+  height: 42px;
+  margin-right: 35px;
+  background-image: ${({ image }) => (image ? `url(${image})` : "none")};
+  background-size: cover;
+  background-position: center;
+  @media (max-width: 767px) {
+    width: 26px;
+    height: 26px;
+    margin-right: 20px;
+  }
+`;
+
+export const Introduce = styled.div`
+  margin-left: 35px;
+  font-size: 42px;
+  align-items: center;
+  @media (max-width: 767px) {
+    font-size: 26px;
+    margin-left: 20px;
+  }
+`;
+
+export const SourceArea = styled.div`
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
+  padding: 5px;
+  background-color: #D1D5DB;
+`;
+
+
